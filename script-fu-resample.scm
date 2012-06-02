cd288411d50d43243350f6698087e9e4120b2b96
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-06-01 18:20:39 -1000

    Comment code.

--- script-fu-resample.scm
+++ script-fu-resample.scm
@@ -6,7 +6,6 @@
 ;
 
 (define (script-fu-resample image drawable framewidth pixwidth)
-
     (let*
         (   ; Construct variables here.
             (contraction 1)
@@ -15,10 +14,14 @@
         )
 
         (gimp-image-undo-group-start image)
+        ; Define "contraction" as the ratio of the desired pixel to the width of the frame.
         (set! contraction (/ framewidth pixwidth))
         (set! imwidth (car (gimp-image-width image)))
+        ; Translate the real-unit contraction into pixel contraction (used by GIMP).
         (set! wentry (round (/ imwidth contraction)))
+        ; Pixelize with square pixels: Non-interactive, image, drawable, pixel width, pixel height.
         (plug-in-pixelize2 1 image drawable wentry wentry)
+        ; Show results.
         (gimp-displays-flush)
         (gimp-image-undo-group-end image)
         (gimp-image-clean-all image)
@@ -35,6 +38,6 @@
     "RGB GRAY INDEXED"
     SF-IMAGE "Image" 0
     SF-DRAWABLE "Drawable" 0
-    SF-VALUE "In real units (m, km, etc.), how wide is your image?" "0"
-    SF-VALUE "In the same units, how wide is your pixel?" "0"    
+    SF-VALUE "In real units (m, km, etc.), how wide is your image" "0"
+    SF-VALUE "In the same units, how wide is your pixel" "0"    
 )
